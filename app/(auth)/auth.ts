a9286dbd78f6eca3f074f50f392d5ffa93fc8dc6
Ricardo <ricardinbaum@gmail.com>
2026-03-30 22:06:10 -0300

    redis fix

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -3,15 +3,29 @@ import { nextCookies } from "better-auth/next-js";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { env } from "@/env";
 import { prisma } from "../(database)/database";
+import { RedisClient } from "bun";
 
-const redis = new Redis({
-  host: "localhost",
-  port: 6379,
-});
+const client = new RedisClient(env.REDIS_URL);
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+	secondaryStorage: {
+		get: async (key) => {
+			return await client.get(key);
+		},
+		set: async (key, value, ttl) => {
+			if (ttl) {
+        await client.set(key, value);
+        await client.expire(key, ttl)
+      }
+			else await client.set(key, value);
+		},
+		delete: async (key) => {
+			await client.del(key);
+		},
+	},
   baseURL: env.BETTER_AUTH_URL,
   experimental: { joins: true },
   socialProviders: {
--- env.ts
+++ env.ts
@@ -7,6 +7,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     DATABASE_URL: z.string().min(1),
+    REDIS_URL: z.string().min(1),
   },
   clientPrefix: "",
   client: {
